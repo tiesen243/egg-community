4a75db23608bdad525fb230294078a1f14c0afb9
tiesen243 <ttien56906@gmail.com>
2024-04-15 19:46:11 +0700

    style: add padding in login form :D

--- app/auth/layout.tsx
+++ app/auth/layout.tsx
@@ -1,5 +1,5 @@
 const AuthLayout: React.FC<React.PropsWithChildren> = ({ children }) => (
-  <div className="flex h-[69dvh] w-full items-center justify-center [&>*]:max-w-screen-md [&>*]:flex-1 [&>*]:space-y-4">
+  <div className="flex h-[69dvh] w-full items-center justify-center [&>*]:container [&>*]:max-w-screen-md [&>*]:flex-1 [&>*]:space-y-4">
     {children}
   </div>
 )
