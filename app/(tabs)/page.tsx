6598b02c9c80d015098c948f14d1734fa8b112a6
tiesen243 <ttien56906@gmail.com>
2024-04-15 16:48:35 +0700

    style: post card

--- app/(tabs)/page.tsx
+++ app/(tabs)/page.tsx
@@ -1,48 +1,25 @@
+import { PostCard } from '@/components/post-card'
 import type { NextPage } from 'next'
+
+const mockData = {
+  author: { id: 'a', name: 'TIesen', avatar: 'https://source.unsplash.com/random' },
+  content:
+    'Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat.',
+  image: 'https://source.unsplash.com/random',
+  createdAt: '2021-10-10T10:00:00Z',
+  heartCount: 10,
+  replyCount: 5,
+}
 
 const Page: NextPage = () => {
   return (
-    <div>
-      Lorem ipsum dolor sit amet, officia excepteur ex fugiat reprehenderit enim labore culpa sint
-      ad nisi Lorem pariatur mollit ex esse exercitation amet. Nisi anim cupidatat excepteur
-      officia. Reprehenderit nostrud nostrud ipsum Lorem est aliquip amet voluptate voluptate dolor
-      minim nulla est proident. Nostrud officia pariatur ut officia. Sit irure elit esse ea nulla
-      sunt ex occaecat reprehenderit commodo officia dolor Lorem duis laboris cupidatat officia
-      voluptate. Culpa proident adipisicing id nulla nisi laboris ex in Lorem sunt duis officia
-      eiusmod. Aliqua reprehenderit commodo ex non excepteur duis sunt velit enim. Voluptate laboris
-      sint cupidatat ullamco ut ea consectetur et est culpa et culpa duis. Lorem ipsum dolor sit
-      amet, officia excepteur ex fugiat reprehenderit enim labore culpa sint ad nisi Lorem pariatur
-      mollit ex esse exercitation amet. Nisi anim cupidatat excepteur officia. Reprehenderit nostrud
-      nostrud ipsum Lorem est aliquip amet voluptate voluptate dolor minim nulla est proident.
-      Nostrud officia pariatur ut officia. Sit irure elit esse ea nulla sunt ex occaecat
-      reprehenderit commodo officia dolor Lorem duis laboris cupidatat officia voluptate. Culpa
-      proident adipisicing id nulla nisi laboris ex in Lorem sunt duis officia eiusmod. Aliqua
-      reprehenderit commodo ex non excepteur duis sunt velit enim. Voluptate laboris sint cupidatat
-      ullamco ut ea consectetur et est culpa et culpa duis. Lorem ipsum dolor sit amet, officia
-      excepteur ex fugiat reprehenderit enim labore culpa sint ad nisi Lorem pariatur mollit ex esse
-      exercitation amet. Nisi anim cupidatat excepteur officia. Reprehenderit nostrud nostrud ipsum
-      Lorem est aliquip amet voluptate voluptate dolor minim nulla est proident. Nostrud officia
-      pariatur ut officia. Sit irure elit esse ea nulla sunt ex occaecat reprehenderit commodo
-      officia dolor Lorem duis laboris cupidatat officia voluptate. Culpa proident adipisicing id
-      nulla nisi laboris ex in Lorem sunt duis officia eiusmod. Aliqua reprehenderit commodo ex non
-      excepteur duis sunt velit enim. Voluptate laboris sint cupidatat ullamco ut ea consectetur et
-      est culpa et culpa duis. Lorem ipsum dolor sit amet, officia excepteur ex fugiat reprehenderit
-      enim labore culpa sint ad nisi Lorem pariatur mollit ex esse exercitation amet. Nisi anim
-      cupidatat excepteur officia. Reprehenderit nostrud nostrud ipsum Lorem est aliquip amet
-      voluptate voluptate dolor minim nulla est proident. Nostrud officia pariatur ut officia. Sit
-      irure elit esse ea nulla sunt ex occaecat reprehenderit commodo officia dolor Lorem duis
-      laboris cupidatat officia voluptate. Culpa proident adipisicing id nulla nisi laboris ex in
-      Lorem sunt duis officia eiusmod. Aliqua reprehenderit commodo ex non excepteur duis sunt velit
-      enim. Voluptate laboris sint cupidatat ullamco ut ea consectetur et est culpa et culpa duis.
-      Lorem ipsum dolor sit amet, officia excepteur ex fugiat reprehenderit enim labore culpa sint
-      ad nisi Lorem pariatur mollit ex esse exercitation amet. Nisi anim cupidatat excepteur
-      officia. Reprehenderit nostrud nostrud ipsum Lorem est aliquip amet voluptate voluptate dolor
-      minim nulla est proident. Nostrud officia pariatur ut officia. Sit irure elit esse ea nulla
-      sunt ex occaecat reprehenderit commodo officia dolor Lorem duis laboris cupidatat officia
-      voluptate. Culpa proident adipisicing id nulla nisi laboris ex in Lorem sunt duis officia
-      eiusmod. Aliqua reprehenderit commodo ex non excepteur duis sunt velit enim. Voluptate laboris
-      sint cupidatat ullamco ut ea consectetur et est culpa et culpa duis.
-    </div>
+    <ul>
+      {[...Array(10).fill(mockData)].map((data, idx) => (
+        <li key={idx}>
+          <PostCard post={data} />
+        </li>
+      ))}
+    </ul>
   )
 }
 
