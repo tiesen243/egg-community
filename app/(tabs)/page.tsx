079ebc9d8bb37caea1f6fbccd7b8e1fe819fb2b5
tiesen243 <ttien56906@gmail.com>
2024-04-15 19:43:08 +0700

    fix: make bug and fix it kkkkkkkk

--- app/(tabs)/page.tsx
+++ app/(tabs)/page.tsx
@@ -2,9 +2,11 @@ import type { NextPage } from 'next'
 
 import { PostCard } from '@/components/post-card'
 import { api } from '@/lib/api'
+import { auth } from '@/server/auth'
 
 const Page: NextPage = async () => {
-  const { data, error } = await api.post.getAll.get()
+  const { user } = await auth()
+  const { data, error } = await api.post.getAll.get({ query: { id: user?.id } })
 
   if (!data || error) return <div>Error: {error.value.message ?? 'Unknown error'}</div>
 
