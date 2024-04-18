4a2b6a23cad44e9643614b3a94a75ffeabbd0b17
tiesen243 <ttien56906@gmail.com>
2024-04-18 07:48:35 +0700

    style: fix some z-index

--- app/(tabs)/layout.tsx
+++ app/(tabs)/layout.tsx
@@ -4,7 +4,7 @@ import { Header } from '@/components/header'
 const TabLayot: React.FC<React.PropsWithChildren> = ({ children }) => (
   <>
     <Header />
-    <main className="container max-w-screen-md pb-16">{children}</main>
+    <main className="container max-w-screen-md pb-16 md:pb-4">{children}</main>
     <Footer />
   </>
 )
