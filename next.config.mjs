74ae5a4c57577e11429fe3b09a683dc3c151a1ac
Tiesen <ttien56906@gmail.com>
2024-05-21 15:41:14 +0700

    chore: enable react strict mode

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,7 @@ await import('./env.mjs')
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
   images: { unoptimized: true },
   eslint: { ignoreDuringBuilds: true },
   typescript: { ignoreBuildErrors: true },
